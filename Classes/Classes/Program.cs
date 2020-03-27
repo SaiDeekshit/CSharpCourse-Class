5d9be9084ceab84837faa67c2540bd3c2377c3c5
Sai Deekshith <msaideekshith@gmail.com>
2020-03-27 07:13:42 +0530

    Classes in csharp

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Classes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Creating instance for first person
+            Person firstPerson = new Person();
+            firstPerson.FirstName = "Rama";
+            firstPerson.LastName = "Krishna";
+            firstPerson.Country = "India";
+
+            //Creating instance for second person
+            Person secondPerson = new Person();
+            secondPerson.FirstName = "Tim";
+            secondPerson.LastName = "David";
+            secondPerson.Country = "Australia";
+
+            Console.WriteLine(firstPerson.FirstName);
+            Console.WriteLine(secondPerson.FirstName);
+        }
+
+    }
+
+    //Class represent person details
+    class Person
+    {
+        string firstName;
+        string lastName;
+        DateTime birthDate;
+        string country;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Country { get; set; }
+    }
+}
